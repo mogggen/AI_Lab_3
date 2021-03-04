52a65a8d1a27b38b599f9ebcc428ec0dead6c795
Morgan Nyman <mornym-9@student.ltu.se>
2021-03-04 11:57:42 +0100

    math done!

---
+++ simulate Grade 3.py
@@ -0,0 +1,23 @@
+#requirements
+charcoalNeededForGrade = 200 #charcoal
+
+woodNeededForCharcoal = 400 #wood
+woodNeededPerBuilding = 10 #wood
+TreesNeededForwood = 80 #trees
+
+#durations
+timeElapsed = 0 #seconds
+timetoCutWood = 30 #seconds
+timeToMakeCharcoal = 30 #seconds
+
+timeToBecomeBuilder = 120 #seconds
+timeToBuildBuilding = 60 #seconds
+
+timetoBecomeExplorer = 60 #seconds
+
+#finishing times
+oneBuilding = 6000 + 60 + 60 #seconds needs 2 wood / 60 seconds
+twoBuildings = 3000 + 120 + 0 #seconds needs 4 wood / 60 seconds
+treeBuildings = 1500 + 180 + 0 #seconds needs 6 wood / 60 seconds
+fourBuildings = 750 + 240 + 0 #seconds neeeds 8 wood / 60 seconds
+fiveBuildings = 375 + 300 + 0 #seconds needs 10 wood / 60 seconds
