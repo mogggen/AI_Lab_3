15638f87bad7c94c726e852b27ebe2c8f8b6ef2d
Morgan Nyman <mornym-9@student.ltu.se>
2021-05-02 11:30:51 +0200

    relevant progress

--- fsm.py
+++ fsm.py
@@ -1,5 +1,9 @@
+import enums
 import pathfinding
+import time
+import main
 
+r = (1, 1), (0, 1), (1, 0), (-1, 1), (1, -1), (-1, 0), (0, -1), (-1, -1)
 
 class BaseState:
     def Enter(self, agent):
@@ -8,10 +12,43 @@ class BaseState:
     def Execute(self, agent):
         pass
 
+    def Exit(self, agent):
+        agent.changeState(IdleState)
+
+
+class IdleState(BaseState):
+    def Enter(self, agent):
+        agent.goalPos = main.trees.pop()
+        agent.changeState(MoveState)
+
 
 class MoveState(BaseState):
     def Enter(self, agent):
-        agent.pathToGoal = pathfinding.getAstarPath(agent)
+        agent.pathToGoal = pathfinding.getAstarPath(agent)[:-1]
+        GoalPosistion = agent.pathToGoal[-1]
 
     def Execute(self, agent):
         agent.move()
+
+
+class ChoppingState(BaseState):
+    def Enter(self, agent):
+        agent.startTime = time.time() + 30
+
+    def Execute(self, agent):
+        if time.time() >= agent.startTime:
+            agent.holding = enums.ItemEnum.tree
+            # don't carry it for now
+            agent.changeState(MoveState)
+
+
+class ExploreState(BaseState):
+    def Enter(self, agent):
+        pass
+
+    def Execute(self, agent):
+        for n in r:
+            if main.karta[agent.pos[0] + n[0], agent.pos[1] + n[1]][0] == 'T':
+                main.trees.append(main.karta[agent.pos[0] + n[0], agent.pos[1] + n[1]])
+
+        agent.pathToGoal = pathfinding.getAstarPath(agent)
--- pathfinding.py
+++ pathfinding.py
@@ -24,3 +24,18 @@ def astar(graph, node, end, g=0, par=None):
         # - 1 + 2 * (par == neighbour)
         for best in improved:
             astar(graph, improved[best], end, g, node)
+
+# for i in range(9):
+#     n = p.X - 1 + i % 3, p.Y - 1 + i / 3 # p is agent
+#     if i == 4:
+#         if karta[n.X, n.Y].land.BackColor == closed: # if the child node is in the openlist
+#             continue
+#         else:
+#             karta[n.X, n.Y].land.BackColor = open # append to openlist
+#             step = 14 if ((p.X - n.X + (p.Y - n.Y)) % 2 == 0) else 10 # straight or diagonal step
+#             if karta[p.X, p.Y].gCost + step < karta[n.X, n.Y].gCost or karta[n.X, n.Y].gCost == 0: # is better or is not the starting node
+#                 karta[n.X, n.Y].gCost = karta[p.X, p.Y].gCost + step # new improved gCost
+#                 karta[n.X, n.Y].parent = b # new parent position
+#
+#             #prints the gCost
+#             karta[n.X, n.Y].fCost = karta[n.X, n.Y].gCost + karta[n.X, n.Y].hCost
