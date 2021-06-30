e90da1fb8994bf4abd6a1c6b072804723d9cdad8
Morgan Nyman <melon21@hotmail.se>
2021-06-30 16:28:47 +0200

    more dumb junk removed

--- enums.py
+++ enums.py
@@ -1,18 +1,18 @@
 class GoalEnum:
-    CHOPPING_GOAL = 0
-    UPGRADING_GOAL = 1
-    SCOUT_GOAL = 2
-    BUILDING_GOAL = 3
+	CHOPPING_GOAL = 0
+	UPGRADING_GOAL = 1
+	SCOUT_GOAL = 2
+	BUILDING_GOAL = 3
 
 
 class AgentEnum:
-    WORKER = 1
-    SCOUT = 2
-    BUILDER = 3
-    MILLER = 4
+	WORKER = 1
+	SCOUT = 2
+	BUILDER = 3
+	MILLER = 4
 
 
 class ItemEnum:
-    none = 0
-    tree = 1
-    charcoal = 2
+	none = 0
+	tree = 1
+	charcoal = 2
